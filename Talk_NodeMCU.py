71c1b521b753d0611a39ffbb5261e12e70e5ce58
anxious-pineapple <46401235+anxious-pineapple@users.noreply.github.com>
2019-07-01 16:13:35 +0530

    Update Talk_NodeMCU.py
    comments

--- Talk_NodeMCU.py
+++ Talk_NodeMCU.py
@@ -1,12 +1,11 @@
 import socket
 import time
 
-HOST = '192.168.0.180'
-  # The server's hostname or IP address
-PORT = 80        # The port used by the server
+HOST = '192.168.0.180'              # The server's hostname or IP address (NodeMCU)
+PORT = 80                           # The port used by the server
 
 def send(message,sock):
-    b=time.time()
+    #b=time.time()
     sock.sendall(message)
     #dealay
     #time.sleep(0.5)
@@ -14,33 +13,24 @@ def send(message,sock):
     #while (s.recv(1024)!=b"got em'"):
         
         #sock.sendall(message)
-    print(time.time()-b)
+    #print(time.time()-b)
     return True
     
     
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)              #attach a socket to port and connect to host
+s.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)                #make sure all messages are instantaneously sent
 s.connect((HOST, PORT))
 while True:
     
     user=input("(E)xit  / pwm+dir  : ")
     a=time.time()
     if len(user)==5:
-        send(str.encode(user),s)
+        send(str.encode(user),s)    #info send as a 5 digit bit string, first four digits for pwm width, last for mode(frwd/bckwd/rght/left)
     elif user=="E":
         break
     else:
         send(b"40400",s)
     print(time.time()-a, " MAIN LOOP TIME")
 
-s.close()
-
-
-    
-    
-    
-
-#print('Received', repr(data))1
-    
-    
+s.close()                                           #disconnect with NodeMCU
